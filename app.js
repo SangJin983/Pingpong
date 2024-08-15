285d23861c7513432e51c4ed2294b2627db37a96
SangJin983 <lazistar983@gmail.com>
2024-08-15 17:19:19 +0900

    playerPaddle 움직이게 하기(리팩토링)

--- app.js
+++ app.js
@@ -2,59 +2,46 @@
 const $canvas = document.querySelector(".pong");
 const ctx = $canvas.getContext("2d");
 
-// 패들 사이즈 설정하기
-const paddleSize = {
-  width: 10,
-  height: 100,
-};
+// 패들 사이즈, 이동 속도 설정
+const paddleWidth = 10;
+const paddleHight = 100;
+const paddleSpeed = 5;
 
 // 패들 만드는 함수 (파라미터로 필요한건 x, y)
 function createPaddle(x, y) {
   return {
     x: x,
     y: y,
-    width: paddleSize.width,
-    height: paddleSize.height,
-    dy: 5, // 움직이는 속도 (y축)
+    width: paddleWidth,
+    height: paddleHight,
+    dy: paddleSpeed, // 움직이는 속도 (y축)
     moveUp: false,
     moveDown: false,
   };
 }
 
-// 패들 만들기(위치만 넣어주면 됨)
-const playerPaddle = createPaddle(
-  0,
-  $canvas.height / 2 - paddleSize.height / 2
-);
+// 패들 초기 위치 설정
+const playerPaddle = createPaddle(0, $canvas.height / 2 - paddleHight / 2);
 const aiPaddle = createPaddle(
-  $canvas.width - paddleSize.width,
-  $canvas.height / 2 - paddleSize.height / 2
+  $canvas.width - paddleWidth,
+  $canvas.height / 2 - paddleHight / 2
 );
 
-// 패들 그리기 함수
+// 패들(직사각형) 그리기 함수
 function drawPaddle(paddle) {
   ctx.fillStyle = "#ff0000";
   ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
 }
 
-// 키 입력 처리
-document.addEventListener("keydown", function (event) {
-  if (event.key === "ArrowUp") {
-    playerPaddle.moveUp = true;
-  }
-  if (event.key === "ArrowDown") {
-    playerPaddle.moveDown = true;
-  }
-});
+// 키 버튼 입력 처리
+function handleKeyPress(event, keyPressed) {
+  if (event.key === "ArrowUp") playerPaddle.moveUp = keyPressed;
+  if (event.key === "ArrowDown") playerPaddle.moveDown = keyPressed;
+}
 
-document.addEventListener("keyup", function (event) {
-  if (event.key === "ArrowUp") {
-    playerPaddle.moveUp = false;
-  }
-  if (event.key === "ArrowDown") {
-    playerPaddle.moveDown = false;
-  }
-});
+// 키 입력 이벤트리스너 통합(누를 때 true, 뗄 때 false)
+document.addEventListener("keydown", (event) => handleKeyPress(event, true));
+document.addEventListener("keyup", (event) => handleKeyPress(event, false));
 
 // 패들 움직이기 함수
 function movePaddle(paddle) {
@@ -71,9 +58,12 @@ function draw() {
   ctx.clearRect(0, 0, $canvas.width, $canvas.height); // 캔버스 지우기(계속 그려야 되니까)
   drawPaddle(playerPaddle);
   drawPaddle(aiPaddle);
-
   movePaddle(playerPaddle);
 }
 
-// 그리기 (60 FPS)
-setInterval(draw, 1000 / 60);
+function gameLoop() {
+  draw();
+  requestAnimationFrame(gameLoop); // 다음 프레임 요청, 재귀함수
+}
+
+gameLoop();
