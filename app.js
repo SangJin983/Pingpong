6c3998d5a5e40f91c17f38206b2af015c3106647
SangJin983 <lazistar983@gmail.com>
2024-08-15 18:52:16 +0900

    공 만들기(형태만)

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 const $canvas = document.querySelector(".pong");
 const ctx = $canvas.getContext("2d");
 
+const canvasHeight = $canvas.height;
+const canvasWidth = $canvas.width;
+
 // 패들 사이즈, 이동 속도 설정
 const paddleWidth = 10;
-const paddleHight = 100;
+const paddleHeight = 100;
 const paddleSpeed = 5;
 
 // 패들 만드는 함수 (파라미터로 필요한건 x, y)
@@ -13,7 +16,7 @@ function createPaddle(x, y) {
     x: x,
     y: y,
     width: paddleWidth,
-    height: paddleHight,
+    height: paddleHeight,
     dy: paddleSpeed, // 움직이는 속도 (y축)
     moveUp: false,
     moveDown: false,
@@ -21,10 +24,10 @@ function createPaddle(x, y) {
 }
 
 // 패들 초기 위치 설정
-const playerPaddle = createPaddle(0, $canvas.height / 2 - paddleHight / 2);
+const playerPaddle = createPaddle(0, canvasHeight / 2 - paddleHeight / 2);
 const aiPaddle = createPaddle(
-  $canvas.width - paddleWidth,
-  $canvas.height / 2 - paddleHight / 2
+  canvasWidth - paddleWidth,
+  canvasHeight / 2 - paddleHeight / 2
 );
 
 // 패들(직사각형) 그리기 함수
@@ -48,16 +51,43 @@ function movePaddle(paddle) {
   if (paddle.moveUp && paddle.y > 0) {
     paddle.y -= paddle.dy;
   }
-  if (paddle.moveDown && paddle.y + paddle.height < $canvas.height) {
+  if (paddle.moveDown && paddle.y + paddle.height < canvasHeight) {
     paddle.y += paddle.dy;
   }
 }
 
+// 공 스펙 설정
+const ballSize = 10;
+const ballSpeed = 4;
+
+// 공 만들기 함수
+function createBall() {
+  return {
+    x: canvasWidth / 2,
+    y: canvasHeight / 2,
+    size: ballSize,
+    dx: ballSpeed,
+    dy: ballSpeed,
+  };
+}
+
+// 공 초기 위치설정
+const ball = createBall();
+
+// 공(원) 그리기 함수
+function drawBall() {
+  ctx.fillStyle = "#00ff00"; 
+  ctx.beginPath(); // 기존 도형과 경로가 겹치지 않게, 새로운 도형을 그릴 때 사용.
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2); // x, y, 반지름, 각도 0에서, 360도
+  ctx.fill();
+}
+
 // 그리기 함수
 function draw() {
-  ctx.clearRect(0, 0, $canvas.width, $canvas.height); // 캔버스 지우기(계속 그려야 되니까)
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight); // 캔버스 지우기(계속 그려야 되니까)
   drawPaddle(playerPaddle);
   drawPaddle(aiPaddle);
+  drawBall();
   movePaddle(playerPaddle);
 }
 
