29c6b624522869cd3ba9135f4498acc547399f66
SangJin983 <lazistar983@gmail.com>
2024-08-15 20:21:00 +0900

    공 움직이게 하기, 충돌 판정, 화면 밖으로 나갈 시 초기화.

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ function drawPaddle(paddle) {
 function handleKeyPress(event, keyPressed) {
   if (event.key === "ArrowUp") playerPaddle.moveUp = keyPressed;
   if (event.key === "ArrowDown") playerPaddle.moveDown = keyPressed;
+  if (event.key === "w") aiPaddle.moveUp = keyPressed; // 임시로 aiPaddle을 player2 처럼 테스트
+  if (event.key === "s") aiPaddle.moveDown = keyPressed; // 임시
 }
 
 // 키 입력 이벤트리스너 통합(누를 때 true, 뗄 때 false)
@@ -65,21 +67,63 @@ function createBall() {
   return {
     x: canvasWidth / 2,
     y: canvasHeight / 2,
-    size: ballSize,
+    radius: ballSize,
     dx: ballSpeed,
     dy: ballSpeed,
   };
 }
 
-// 공 초기 위치설정
+// 공 초기 위치설정(공 셋팅하기)
 const ball = createBall();
 
 // 공(원) 그리기 함수
 function drawBall() {
-  ctx.fillStyle = "#00ff00"; 
+  ctx.fillStyle = "#00ff00";
   ctx.beginPath(); // 기존 도형과 경로가 겹치지 않게, 새로운 도형을 그릴 때 사용.
-  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2); // x, y, 반지름, 각도 0에서, 360도
+  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2); // 반지름, 각도 0에서, 360도
   ctx.fill();
+}
+
+// 공과 패들 충돌 확인하는 함수
+function isCollision(paddle, ball) {
+  return (
+    ball.x - ball.radius < paddle.x + paddle.width &&
+    ball.x + ball.radius > paddle.x &&
+    ball.y + ball.radius > paddle.y &&
+    ball.y - ball.radius < paddle.y + paddle.height
+  );
+}
+
+// 움직이는 공 리셋하는 함수
+function resetBall() {
+  ball.x = canvasWidth / 2;
+  ball.y = canvasHeight / 2;
+  ball.dx = ballSpeed * (Math.random() > 0.5 ? 1 : -1);
+  ball.dy = ballSpeed * (Math.random() > 0.5 ? 1 : -1);
+}
+
+// 공 움직이기 함수
+function moveBall() {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  // 벽과 충돌했을 때의 처리
+  if (ball.y - ball.radius < 0 || ball.y + ball.radius > canvasHeight) {
+    ball.dy = -ball.dy;
+  }
+
+  // 패들과 충돌 했을 때의 처리
+  if (isCollision(playerPaddle, ball)) {
+    ball.dx = -ball.dx;
+  }
+  if (isCollision(aiPaddle, ball)) {
+    ball.dx = -ball.dx;
+  }
+
+  // 공이 화면에 넘어가면 초기화
+  if (ball.x + ball.radius < 0 || ball.x - ball.radius > canvasWidth) {
+    resetBall();
+  }
 }
 
 // 그리기 함수
@@ -88,7 +132,10 @@ function draw() {
   drawPaddle(playerPaddle);
   drawPaddle(aiPaddle);
   drawBall();
+
   movePaddle(playerPaddle);
+  movePaddle(aiPaddle); // 임시로 player2 처럼 조작
+  moveBall();
 }
 
 function gameLoop() {
